44acc04ba4dfd4f2f091fd44885f46839942b93e
Tianyu Gu <macdavid313@gmail.com>
2024-04-05 10:40:04 +1100

    add "CSES Problem Set" section

---
+++ CSES Problem Set/Introductory Problems/missing-number.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  int xnum = 0;
+  for (int i = 1; i <= N; i++) {
+    xnum ^= i;
+  }
+
+  int x;
+  for (int i = 0; i < N - 1; i++) {
+    cin >> x;
+    xnum ^= x;
+  }
+
+  cout << xnum << endl;
+}
---
+++ CSES Problem Set/Introductory Problems/weird-algorithms.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long long N;
+  cin >> N;
+  while (N != 1) {
+    cout << N << " ";
+    if (N % 2 == 0) {
+      N = N / 2;
+    } else {
+      N = N * 3 + 1;
+    }
+  }
+  cout << 1 << "\n";
+}
